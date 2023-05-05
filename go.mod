15fc99aa1b9cc42d09b4ab594d3e7d22d4e272dc
Diego Palacio <palaciodiego099@gmail.com>
2023-05-05 15:09:35 -0500

    Added main sniffing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sniffing_go
+
+go 1.19
+
+require github.com/google/gopacket v1.1.19
--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	devName string = "eth0"
+	found   bool   = false
+)
+
+func main() {
+	// Find all network interfaces for package capturing using pcap.FindAllDevs()
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panic("error finding network interfaces: %w", err.Error())
+	}
+
+	for _, device := range devices {
+		if device.Name == devName {
+			found = true
+			break
+		}
+	}
+
+	// If the desired device if foubd continue other exit the program
+	if !found {
+		log.Panic("Error finding network interface: %w", devName)
+	}
+
+	// Open the device for capturing using pcap.OpenLive()
+	handle, err := pcap.OpenLive(devName, 1060, false, pcap.BlockForever)
+	if err != nil {
+		log.Panic("Error opening hnadle on the device: %w", err.Error())
+	}
+	defer handle.Close()
+
+	// Apply BPF filter to the device on this new handle using pcap.SetBPFFilter()
+	if err := handle.SetBPFFilter("tcp and port 443"); err != nil {
+		log.Panic("Error setting BPF filter: %w", err.Error())
+	}
+
+	// Show all the filtered packets received on channel returned from gopacket.NewPacketSource()
+	source := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range source.Packets() {
+		fmt.Println(packet)
+	}
 }
